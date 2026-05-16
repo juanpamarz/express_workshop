b131349816325917848419571d4aa644a9cdf0aa
juanpamarz <jmonroy26@alumnos.uaq.mx>
2026-05-15 21:04:58 -0600

    Operador ternario y metodo filter

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 const express = require ('express');
 const app = express();
-const {pokemon} = require('./pokedex.json');
+const { pokemon } = require('./pokedex.json');
 
-app.get ("/", (req, res, next)=>{
-    res.status(200);
-    res.send("Bienvenido a la Pokedex");
+// GET ES PARA OBTENER RECURSOS
+// POST ES PARA ALMACENAR RECURSOS O CREAR RECURSOS
+// PUT ES MODIFICAR RECURSOS
+// PATCH PARA MODIFICAR PARCIALMENTE RECURSOS
+// DELETE ES PARA ELIMINAR RECURSOS
+
+app.get ("/", (req, res, next) => {
+    res.status(200).send("Bienvenido a la Pokedex");
 });
 
-
-app.get("/pokemon/all", (req, res, next)=>{
+app.get("/pokemon", (req, res, next) => {
     res.status(200);
     res.json(pokemon);
 });
-//HICE MODIFICACIONES EN EL APP GET PORQUE LAS VERSIONES DE EXPRESS YA NO ACEPTABAN LO QUE DICE EL VIDEO. 
+
 app.get("/pokemon/:id", (req, res, next) => {
     const parametro = req.params.id;
 
+    // 1. SI ES UN NOMBRE (Filtramos usando .find())
     if (isNaN(parametro)) {
         const encontrado = pokemon.find(p => p.name.toLowerCase() === parametro.toLowerCase());
         
-        if (encontrado) {
-            return res.json(encontrado);
-        } else {
-            return next();
-        }
+        // Operador Ternario: Si lo encuentra responde JSON, si no, salta al siguiente middleware (next)
+        return encontrado ? (console.log("Encontrado por nombre:", encontrado), res.json(encontrado)) : next();
     }
 
+    // 2. SI ES UN ID (Es un número)
     const id = parseInt(parametro, 10) - 1;
 
-    if (id >= 0 && id < pokemon.length) {
-        return res.json(pokemon[id]);
-    } else {
-        return next();
-    }
+    // Operador Ternario para la validación del rango del ID
+    const idValido = (id >= 0 && id < pokemon.length);
+    
+    return idValido ? (console.log("Encontrado por ID:", pokemon[id]), res.json(pokemon[id])) : next();
 });
 
-
-app.listen(process.env.PORT ||3000, ()=>{
+app.listen(process.env.PORT || 3000, () => {
     console.log("ya corrió...");
 });
