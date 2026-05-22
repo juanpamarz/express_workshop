3d3fe15d57e19a41c52b86d26891afa275105a42
juanpamarz <jmonroy26@alumnos.uaq.mx>
2026-05-22 12:37:15 -0600

    jsonwebtoken

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const bodyParser = require('body-parser');
+
 const morgan = require('morgan');
-const express = require ('express');
+const express = require("express");
 const app = express();
+
+// Routers
 const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
+// Middleware
+const auth = require('../middleware/auth');
+const notFound = require('../middleware/notFound');
+const indexDos = require('../middleware/indexDos');
 
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-app.get ("/", (req, res, next) => {
-    res.status(200).json({ code: 1, message: "Bienvenido a la Pokedex" });
-});
+// Rutas Públicas
+app.get("/", indexDos);
+app.use("/user", user);
 
+// Rutas Protegidas (Requieren Token)
+app.use(auth);
 app.use("/pokemon", pokemon);
 
-app.use((req, res, next) => {
-    return res.status(404).json({ code:404, message: "URL no encontrado" });
-});
-
-
+// Manejador final
+app.use(notFound);
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("ya corrió...");
+    console.log("Server is running...");
 });
