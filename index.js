5311c890b214d4d645f2a964700f02349e83bb9c
juanpamarz <jmonroy26@alumnos.uaq.mx>
2026-05-22 13:40:34 -0600

    Implemetando JWT

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
-
 const morgan = require('morgan');
 const express = require("express");
 const app = express();
+const jwt = require('jsonwebtoken'); // Necesitas importar jwt aquí
 
-// Routers
 const pokemon = require('./routes/pokemon');
 const user = require('./routes/user');
-// Middleware
-const auth = require('../middleware/auth');
-const notFound = require('../middleware/notFound');
-const indexDos = require('../middleware/indexDos');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-// Rutas Públicas
-app.get("/", indexDos);
-app.use("/user", user);
+app.get("/", (req, res, next) => {
+    return res.status(200).json({ code: 1, message: "Bienvenido al Pokédex" });
+});
 
-// Rutas Protegidas (Requieren Token)
-app.use(auth);
+app.use("/user", user); // Registro y Login público
+
+// Middleware de seguridad (inline, como en tu captura)
+app.use((req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decoded = jwt.verify(token, "debugkey");
+        req.user = decoded;
+        next();
+    } catch (error) {
+        return res.status(401).json({ code: 401, message: "No tienes permiso :(" });
+    }
+});
+
+// Ahora sí, esta ruta está protegida por el middleware de arriba
 app.use("/pokemon", pokemon);
 
-// Manejador final
-app.use(notFound);
+app.use((req, res, next) => {
+    return res.status(404).json({ code: 404, message: "URL no encontrada" });
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log("Server is running...");
