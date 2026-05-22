5d3005b4e8babfa1bc822fb5c0184fa49095590b
juanpamarz <jmonroy26@alumnos.uaq.mx>
2026-05-22 09:06:44 -0600

    POST EN POKEMON

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -3,25 +3,34 @@ const pokemon = express.Router();
 const db = require('../config/database');
 
 // POST: Almacenar o recibir recursos
-pokemon.post("/", (req, res, next) => {
-    return res.status(200).json({ nombreRecibido: req.body.name });
+// POST: Almacenar o recibir recursos
+pokemon.post("/", async (req, res, next) => {
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
+    let query = "INSERT INTO pokemon (pok_name, pok_height, pok_weight, pok_base_experience)";
+    query += ` VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;    console.log("Cuerpo recibido en el servidor:", req.body);
+    
+    const rows = await db.query(query);
+    if(rows.affectedRows == 0) {
+        return res.status(200).json(query);
+    }
+    //return res.status(200).json(req.body);
 });
 
 // GET: Obtener todos los pokemon
 pokemon.get("/", async(req, res, next) => {
     const pkmn = await db.query("SELECT * FROM pokemon");
-    return res.status(200).json({ code: 1, message: pkmn }); 
+    return res.status(201).json({ nombreRecibido: req.body.name });
 });
 
 pokemon.get("/:id", async (req, res, next) => {
     const parametro = req.params.id;
 
-    // 1. SI ES UN NOMBRE (Como la ruta de la línea 23 del profe)
+    // 1. SI ES UN NOMBRE
     if (isNaN(parametro)) {
         const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name = '" + parametro + "';");
         
         if (pkmn.length > 0) {
-            return res.status(200).json({ code: 1, message: pkmn[0] });
+            return res.status(201).json({ code: 1, message: pkmn[0] });
         }
         return res.status(404).json({ code: 404, message: "Pokémon no encontrado" });
     }
